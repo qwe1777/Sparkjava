efdf6b841ffb7da316a47a59c74286c0d1cd7589
Maheswar Goud <73587215+maheswargoud@users.noreply.github.com>
2022-03-06 17:07:57 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "TEsting CICD");
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
